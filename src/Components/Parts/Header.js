113e5566d9ef80a7f8d76b4eefbe3aeb720df8c1
beyondsundae <p00000rice@gmail.com>
2020-12-23 00:41:34 +0700

    Updated Header to responsive and many interface, but not done yet, Keep fghtin im almost goal \(-v -)/ zzZZ

--- src/Components/Parts/Header.js
+++ src/Components/Parts/Header.js
@@ -2,48 +2,100 @@ import React, { useEffect, useContext, useState } from 'react'
 
 import app, { firestore, storage } from "../../Firebase/firebase"
 
+import { Avatar, Menu, Divider, Button, Badge } from "antd";
+import { CaretDownFilled, ShoppingCartOutlined } from "@ant-design/icons";
+ 
 import { AuthContext } from "../Auth"
 
+var _ = require('lodash');
 function Header() {
+    const {width} = useWidth()
+    const breakPoint = 500
+
+    const { SubMenu } = Menu;
+
+    const [ pixelValue, setPixelValue ] = useState("")
+
+    const Arrow = <CaretDownFilled style={{ fontSize: '20px'}}/>
+    const Cart = <ShoppingCartOutlined style={{ fontSize: '30px' }}/>
+
     const { currentUser, userData, allEmployees } = useContext(AuthContext)
 
+    const FilterByNotDone = _.filter(userData.queue, ['status', "NotDone"])// Filter หาที่มี status เป็น NotDone
+
+    useEffect(() => {
+        
+
+    }, [])
+
     return (
-        <div className="row">
-                    <div className="col align-self-end">
-                            <h2 className="d-inline border border-danger ">
-                                {userData? ( userData.role == "employee" ? ("น้อง " + userData.displayName) : ("คุณ " + userData.displayName)): "ยังไม่ได้เข้าสู่ระบบ"}
-                            </h2>
-                        </div>
+        <div className="row ">
+                    <div className="col- col-sm-2 col-md-5 col-lg-5 col-xl-9">
+                    </div>
 
-                        <div className="align-self-end">
-                            <button className="mt-3 mr-3 btn btn-warning " >
-                                <a href="/booking" className="text-dark" style={{textDecoration: "none"}}>
-                                    การจองของคุณ
+                    <div className="col-3 col-sm-2 col-md-2 pl-4 col-lg-2 col-xl-1 " style={{left: (width < 500 ? ("-20px") : ("190px") )}}>
+                            <Button size="large" className="mt-2 pt-3 mb- mr-4 text-white" style={{ border: "1px solid transparent", background: 'transparent'}}>
+                                <a href="/booking" >
+                                    <Badge className="text-white" count={FilterByNotDone.length}>
+                                        {Cart}
+                                    </Badge>
                                 </a>
-                            </button>
+                            </Button>
+                    </div>
 
-                            <button className="mt-3 mr-3 btn btn-primary " >
-                                <a href="/profile" className="text-dark" style={{textDecoration: "none"}}>
-                                    Profile
-                                </a>
-                            </button>
-                        </div>
-                        
-                        <div className="align-self-end">
-                            {currentUser? (
-                                <button className="mt-3 btn btn-danger" onClick={() => {app.auth().signOut()}}>
-                                    Log Out
-                                </button>
-                            ) : (
-                                <button className="mt-3 btn btn-info " >
-                                    <a href="/login" className="text-light" style={{textDecoration: "none"}}>
-                                        Log In
-                                    </a>
-                                </button>
-                            )}
+                        <div className="col-9 col-sm-2 col-md-5 pl-3 col-lg-5 col-xl-2" >
+                            <div className="">
+                                {currentUser? (
+                                    <Menu
+                                    // onClick={this.handleClick}
+                                    // selectedKeys={[current]}
+                                    mode="horizontal"
+                                    className="mr-5 mt-2 text-right "
+                                    style={{border: "1px solid transparent", background: 'transparent', color: "white", paddingLeft: (width < 500 ? ("115px") : ("150px") )}}
+                                    > 
+                                        <SubMenu key="SubMenu" 
+                                        icon={
+                                            <div className="d-inline ">
+                                                <Avatar src={userData.urlPhoto} size="large" shape="square" />
+                                                {(userData? ( 
+                                                    userData.role == "employee" ? (
+                                                        <h4 className="d-inline ml-2 text-white">น้อง  { userData.displayName} </h4> ) : (
+                                                        <p className="d-inline ml-2 text-white">คุณ  { userData.displayName} </p>)): null)}
+                                            </div>} 
+                                        title={<div className="d-inline ml-2">{Arrow}</div>}>
+                                            <Menu.Item key="setting:1" ><a href="/profile"  >โปรไฟล์</a></Menu.Item>
+                                            <Menu.Item key="setting:2" onClick={() => {app.auth().signOut()}}>Log out</Menu.Item>
+                                        </SubMenu>
+                                    </Menu>
+                                ) : (
+                                    <button className="mt-3 btn btn-info " >
+                                        <a href="/login" className="text-light" style={{textDecoration: "none"}}>
+                                            Log In
+                                        </a>
+                                    </button>
+                                )}
+                            </div>
                         </div>
                 </div>
     )
 }
 
 export default Header
+
+const useWidth = () => {
+    const [ width, setWidth ] = useState(window.innerWidth)
+
+    const widthHandler =()=>{
+        setWidth(window.innerWidth)
+    }
+
+    useEffect(()=>{
+        window.addEventListener("resize", widthHandler)
+
+        return()=>{
+            window.removeEventListener("resize", widthHandler)
+        }
+    }, [])
+
+    return { width };
+}
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -4,33 +4,66 @@ import app, { firestore, storage } from "../Firebase/firebase"
 
 import Header from "./Parts/Header"
 
+import { Menu, Button, Divider, Descriptions } from "antd";
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    PieChartOutlined,
+    DesktopOutlined,
+    ContainerOutlined,
+  } from "@ant-design/icons";
+import Rating from "@material-ui/lab/Rating";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { withStyles } from "@material-ui/core/styles";
+
 import { AuthContext } from "./Auth"
 
-
+var _ = require('lodash');
 function Profile() {
     const { currentUser, userData, Modal } = useContext(AuthContext)
 
     const [isModalVisible, setIsModalVisible] = useState(false);
-    const [statusButt, setstatusButt] = useState({ disabled: false });
+    const [statusButt, setstatusButt] = useState(false);
     const [ progress, setProgress ] = useState(false)
 
     const [ pic, setPic ] = useState("")
-
     const [ loadingPic, setLoadingPic ] = useState("true")
+    const [ changeName, setchangeName ] = useState("")
 
-    const [ changeDName, setchangeDName ] = useState("")
+    const [ collapse, setCollapse ] = useState(true)
+
+
+    const QueueOrderedDESC =  _.orderBy(userData.queue, ["Date", "Time"], ["desc", "desc"])// เรียงวันล่าสุดมาก่อน
+    const FilterByDone = _.filter(QueueOrderedDESC, ['status', "Done"])// Filter หาที่มี status เป็น Done
+        const FilterReviewed = FilterByDone.filter(item => {
+        return ![{totalStar: null}].some(NullStar => NullStar.totalStar === item.Review.totalStar)
+    })// Different หาคืวที่มีการให้คะแนนแล้ว เพื่อจะเอาไปแสดงในช่องรีวิว
+            const sumStar = FilterReviewed.reduce((prev, item)=>{
+                return(item.Review.totalStar + prev )
+            }, 0)// sum star ที่งหมด
+                const finalStar = (sumStar/FilterReviewed.length).toFixed( 1 ) // หารให้เต็ม 5 
 
     const Style = {
         Header: {
-            height: "7vh"
+            height: "8vh",
+            background: '#595959'
         },
         // preContent: {
         //     height: "30vh"
         // },
         Content: {
-            minHeight: "92vh"
+            minHeight: "91vh"
         }
     }
+
+    const StyledRating = withStyles({
+        iconFilled: {
+          color: "#ff6d75"
+        },
+        iconHover: {
+          color: "#ff3d47"
+        }
+      })(Rating);
 
     const tempPic = () => {
         setTimeout(() => {
@@ -38,15 +71,15 @@ function Profile() {
         }, 1000);
     }
 
-    const handleChangeDName =  (e) => {
-        setchangeDName(e.target.value)
+    const handlechangeName =  (e) => {
+        setchangeName(e.target.value)
     }
 
     const submitNewName = async (e) => {
         e.preventDefault();
 
         setProgress(true)
-        setstatusButt({ disabled: true })
+        setstatusButt(true)
 
         try{
 
@@ -58,37 +91,38 @@ function Profile() {
 
            const obj = {
                ...objDoc,
-               displayName: changeDName
+               displayName: changeName
            }
-            userRef.set(obj)
+            await userRef.set(obj)
 
             setTimeout(() => {
-                setchangeDName('')
+                setchangeName('')
 
                 setIsModalVisible(false);
                 setProgress(false)
-                setstatusButt({ disabled: false })
-            }, 2000);
+                setstatusButt(false)
+            }, 1000);
             
 
         } catch(err) {
             console.log(err)
-
         }
         
     }
 
     const showModal = (e) => {
-        e.preventDefault();
         setIsModalVisible(true);
         };
     const handleCancel = () => {
         setIsModalVisible(false);
         };
+    
+    const toggleCollapsed = () => {
+        setCollapse(!collapse)
+    }
 
     useEffect(() => {
-    //   console.log(currentUser.email)
-    //   console.log(pic)
+    //   console.log()
 
       if(userData.role){
         storage.child(userData.role + "/" + currentUser.uid + "/ProfilePic.jpg")
@@ -106,64 +140,136 @@ function Profile() {
     
     return (
         <div style={{textAlign: "center"}}>
-            <div className="container-fluid text-right border border-danger " style={Style.Header}>
+            <div className="container-fluid text-right " style={Style.Header}>
                 <Header />
             </div>
 
             <div className="container-fluid mt-1 text-center border border-danger" style={Style.Content}>
-                {pic ? (
-                    loadingPic ? null : ( <img src={pic} style={{width: "20%"}} />) 
-                )  
-                : (
-                    loadingPic ? null : ( <img src="https://icons-for-free.com/iconfiles/png/512/instagram+person+profile+icon-1320184028516722357.png" style={{width: "10%"}}/> ) 
-                )}
-            {/* still have unset picture delay */}
-            
-                <h1>Profile</h1>
-                <h2>{currentUser.email}</h2><br/>
-                <button className="mt-3 btn btn-info" >
-                    <a href="/admin" className="text-light" style={{textDecoration: "none"}}>
-                    เปลี่ยนรูป 
-                    </a>
-                </button><br></br>
-                <button className="mt-3 btn btn-warning" >
-                    <a href="/masssagelists" className="text-light" style={{textDecoration: "none"}}>
-                    จัดการรายการนวด 
-                    </a>
-                </button>
+                <div  className="row">
+                    <div className="col-2 ">
+                        <div className="text-left" style={{ width: 256 }}>
+                            <Button
+                            type="primary"
+                            onClick={toggleCollapsed}
+                            style={{ marginBottom: 16 }}
+                            >
 
-                <form onSubmit={(e) => showModal(e)}>
-                    <button className=" btn btn-success my-3" type='submit'>เปลี่ยนชื่อ</button>
-                </form>
+                            {React.createElement(
+                                collapse ? MenuUnfoldOutlined : MenuFoldOutlined
+                            )}
 
-                <h1>Data Firestore</h1>
-                <h2>uid: { userData? userData.uid : null }</h2>
-                <h2>displayName: { userData? userData.displayName : null}</h2>
-                <h2>email: { userData? userData.email : null}</h2>
-                <h2>role: { userData? userData.role : null}</h2>
-                <br/>
+                            </Button>
+                            <Menu
+                            defaultSelectedKeys={["1"]}
+                            defaultOpenKeys={["sub1"]}
+                            mode="inline"
+                            theme="dark"
+                            inlineCollapsed={collapse}
+                            >
+                                <Menu.Item key="1" icon={<PieChartOutlined />}>
+                                    <a href="/admin" className="text-light" style={{textDecoration: "none"}}>
+                                        เปลี่ยนรูป 
+                                    </a>
+                                </Menu.Item>
 
-                <Modal
-                    title="Change Name"
-                    visible={isModalVisible}
-                    onOk={(e)=>submitNewName(e)}
-                    onCancel={()=>handleCancel()}
-                    closable={false}
-                    okButtonProps={ statusButt }
-                    cancelButtonProps={ statusButt }
-                >
-                    <div className="text-center">
-                        <input type="text" value={changeDName} onChange={(e) => handleChangeDName(e)} placeholder="New Name" />
+                                <Menu.Item key="2" onClick={showModal} icon={<DesktopOutlined />}>
+                                        เปลี่ยนชื่อ
+                                </Menu.Item>
 
-                        {progress ? (
-                            <div className="text-center">
-                                <div className="spinner-border mt-5 my-3" role="status"/><br/>
-                                <span className="">Uploading...</span>
-                            </div>
-                            
-                        ) : null}
+                                {userData.role === "employee"? (
+                                    <Menu.Item key="3" icon={<ContainerOutlined />}>
+                                        <a href="/masssagelists" className="text-light" style={{textDecoration: "none"}}>
+                                            จัดการรายการนวด 
+                                        </a>
+                                    </Menu.Item>
+                                ) : (null)}
+                            </Menu>
+                        </div>
                     </div>
-                </Modal>
+
+                    <div className="col-10 border border-danger">
+                        <div className="row">
+                            <div className="col-12 my-5 text-left">
+                              <h3>ข้อมูลของฉัน</h3>  
+                              <Divider />
+                            </div>
+
+                            <div className="col-5">
+                                {pic ? (
+                                    loadingPic ? null : ( <img src={pic} style={{width: "70%"}} />) 
+                                )  
+                                : (
+                                    loadingPic ? null : ( <img src="https://icons-for-free.com/iconfiles/png/512/instagram+person+profile+icon-1320184028516722357.png" style={{width: "10%"}}/> ) 
+                                )}
+                                
+                            </div>
+                            <Divider type="vertical" style={{border: "1px solid rgb(240, 240, 240)", height: "40vh"}}/>
+
+                            <div className="col-6 pl-5 ">
+                                <Descriptions title="" className="" style={{marginLeft: "10vh"}}>
+                                    <Descriptions.Item label="ชื่อผู้ใช้">{ userData? userData.displayName : null}</Descriptions.Item><br/><br/>
+                                    <Descriptions.Item label="อายุ">{userData.age? userData.age : "NaN"}</Descriptions.Item><br/><br/>
+                                    <Descriptions.Item label={userData.role === "member"? "จำนวนที่เข้าใช้บริการ" : "จำนวนผู้เข้าใช้บริการ"}>{ userData? FilterReviewed.length : null}</Descriptions.Item><br/><br/>
+                                    
+                                    {userData.role !== "member"? (
+                                    <>
+                                        <Descriptions.Item label="คะแนน: ">{FilterReviewed.length !== 0?(
+                                            <p style={{color: "#ff6d75"}}>{finalStar} : { userData? (<StyledRating
+                                                className=""
+                                                size="small"
+                                                precision={0.1}
+                                                value={finalStar}
+                                                icon={<FavoriteIcon fontSize="inherit" />}
+                                                readOnly
+                                            />) : null}</p>):(<p>ยังไม่มีการรีวิว</p>)}
+                                        </Descriptions.Item><br/><br/>
+                                        </>) : null}
+                                    
+                                </Descriptions>
+
+                                <Descriptions title="" className="mt-4" style={{marginLeft: "10vh"}}>
+                                    <Descriptions.Item label="Email">{currentUser.email.slice(0,1)+"***"+currentUser.email.slice(4)}</Descriptions.Item><br/><br/>
+                                    <Descriptions.Item label="UID">{ userData? userData.uid.slice(0,6)+"***"+userData.uid.slice(9) : null }</Descriptions.Item>
+                                </Descriptions>
+                            </div>
+                        </div>
+                        
+                    {/* still have unset picture delay */}
+
+                        <Modal
+                            title="เปลี่ยนชื่อ"
+                            visible={isModalVisible}
+                            onOk={(e)=>submitNewName(e)}
+                            onCancel={()=>handleCancel()}
+                            closable={false}
+                            okButtonProps={ statusButt }
+                            cancelButtonProps={ statusButt }
+                            footer={[
+                                <Button key="back" loading={statusButt} onClick={()=>handleCancel()}>
+                                  ย้อนกลับ
+                                </Button>,
+                                <Button key="submit" type="primary" loading={statusButt} onClick={(e)=>submitNewName(e)}>
+                                  ตกลง
+                                </Button>,
+                              ]}
+                        >
+                            <div className="text-center">
+                                <input type="text" value={changeName} onChange={(e) => handlechangeName(e)} placeholder="ชื่อที่ต้องการเปลี่ยน" />
+
+                                {progress ? (
+                                    <div className="text-center">
+                                        <div className="spinner-border mt-5 my-3" role="status"/><br/>
+                                        <span className="">Uploading...</span>
+                                    </div>
+                                    
+                                ) : null}
+                            </div>
+                        </Modal>
+                    </div>
+
+                </div>
+
+                
             </div>
         </div>
     )
